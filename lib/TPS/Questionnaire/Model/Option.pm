7b72179dde5e2c9a9d658e83b9d6873bcf3ea0e9
Toby Inkster <github@toby.ink>
2021-10-07 14:29:29 +0100

    Documentation for the Option class and Question role

--- lib/TPS/Questionnaire/Model/Option.pm
+++ lib/TPS/Questionnaire/Model/Option.pm
@@ -7,6 +7,31 @@ use namespace::autoclean;
 
 use constant { true => !!1, false => !!0 };
 
+=encoding utf-8
+
+=head1 NAME
+
+TPS::Questionnaire::Model::Question - represents a question
+
+=head1 DESCRIPTION
+
+This role represents a question which may belong to a questionnaire.
+
+It is consumed by question type classes.
+
+=head1 ATTRIBUTES
+
+=head2 id
+
+Integer ID for the option. Will not be defined if the option is
+not yet stored in the database.
+
+C<clear_id> may be used to clear an existing ID, allowing it to be re-stored
+in the database with a different ID. C<has_id> indicates if this question
+has an ID.
+
+=cut
+
 has id => (
     is => 'rw',
     isa => 'Int',
@@ -14,11 +39,26 @@ has id => (
     predicate => 'has_id',
 );
 
+=head2 option_text
+
+Required string; the text of the option.
+
+=cut
+
 has option_text => (
     is => 'rw',
     isa => 'Str',
     required => true,
 );
+
+=head1 METHODS
+
+=head2 from_hashref(\%data)
+
+An alternative constructor which creates an option object from a
+hashref of data, formatted in the same manner the API accepts.
+
+=cut
 
 sub from_hashref {
     my ($class, $data) = (shift, @_);
@@ -29,6 +69,12 @@ sub from_hashref {
 
     return $self;
 }
+
+=head2 to_hashref
+
+Creates a hashref of data for this option, suitable for JSON serialization.
+
+=cut
 
 sub to_hashref {
     my ($self) = (shift);
@@ -41,6 +87,14 @@ sub to_hashref {
     return $h;
 }
 
+=head2 from_db_object($schema, $result)
+
+Alternative constructor which creates an option object from a
+L<TPS::Questionnaire::Schema> object and the options's
+L<TPS::Questionnaire::Schema::Result::Option> object.
+
+=cut
+
 sub from_db_object {
     my ($class, $schema, $result) = (shift, @_);
 
@@ -51,6 +105,12 @@ sub from_db_object {
 
     return $self;
 }
+
+=head2 _save($question, $rank, $schema)
+
+Used by L<TPS::Questionnaire::Model::Question>.
+
+=cut
 
 sub _save {
     my ($self, $question, $rank, $schema) = (shift, @_);
@@ -67,6 +127,17 @@ sub _save {
 
     return $self->id;
 }
+
+=head1 AUTHOR
+
+Toby Inkster, tinkster@theperlshop.net
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
 __PACKAGE__->meta->make_immutable;
 
--- lib/TPS/Questionnaire/Model/Question.pm
+++ lib/TPS/Questionnaire/Model/Question.pm
@@ -7,7 +7,43 @@ use namespace::autoclean;
 
 use constant { true => !!1, false => !!0 };
 
+=encoding utf-8
+
+=head1 NAME
+
+TPS::Questionnaire::Model::Question - represents a question
+
+=head1 DESCRIPTION
+
+This role represents a question which may belong to a questionnaire.
+
+It is consumed by question type classes.
+
+=head1 REQUIRED METHODS
+
+These methods are not provided by the role, but must be provided by classes
+that consume it.
+
+=head2 question_type
+
+The question type code for storing the question in the database.
+
+=cut
+
 requires qw(question_type);
+
+=head1 ATTRIBUTES
+
+=head2 id
+
+Integer ID for the question. Will not be defined if the question is
+not yet stored in the database.
+
+C<clear_id> may be used to clear an existing ID, allowing it to be re-stored
+in the database with a different ID. C<has_id> indicates if this question
+has an ID.
+
+=cut
 
 has id => (
     is => 'rw',
@@ -16,11 +52,28 @@ has id => (
     predicate => 'has_id',
 );
 
+=head2 question_text
+
+Required string; the text of the question.
+
+=cut
+
 has question_text => (
     is => 'rw',
     isa => 'Str',
     required => true,
 );
+
+=head1 METHODS
+
+=head2 from_hashref(\%data)
+
+An alternative constructor which creates a question object from a
+hashref of data, formatted in the same manner the API accepts.
+This is a good target for modifying in consuming classes if the question type
+needs to read additional data, such as using a Moose C<around> modifier.
+
+=cut
 
 sub from_hashref {
     my ($class, $data) = (shift, @_);
@@ -31,6 +84,14 @@ sub from_hashref {
 
     return $self;
 }
+
+=head2 to_hashref
+
+Creates a hashref of data for this question, suitable for JSON serialization.
+This is a good target for modifying in consuming classes if the question type
+needs to store additional data, such as using a Moose C<around> modifier.
+
+=cut
 
 sub to_hashref {
     my ($self) = (shift);
@@ -43,6 +104,16 @@ sub to_hashref {
     return $h;
 }
 
+=head2 from_db_object($schema, $result)
+
+Alternative constructor which creates a question object from a
+L<TPS::Questionnaire::Schema> object and the question's
+L<TPS::Questionnaire::Schema::Result::Question> object.
+This is a good target for modifying in consuming classes if the question type
+needs to read additional data, such as using a Moose C<around> modifier.
+
+=cut
+
 sub from_db_object {
     my ($class, $schema, $result) = (shift, @_);
 
@@ -53,6 +124,12 @@ sub from_db_object {
 
     return $self;
 }
+
+=head2 _save($questionnaire, $rank, $schema)
+
+Used by L<TPS::Questionnaire::Model::Questionnaire>.
+
+=cut
 
 sub _save {
     my ($self, $questionnaire, $rank, $schema) = (shift, @_);
@@ -76,5 +153,16 @@ sub _save {
 
     return $self->id;
 }
+
+=head1 AUTHOR
+
+Toby Inkster, tinkster@theperlshop.net
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
 1;
